d04a57758087c9b6bde19b5fb5432fd4057a76ed
Mario_Chiaparini <45772937+MarioChiaparini@users.noreply.github.com>
2022-08-31 13:47:44 -0300

    Update autodock.py

--- autodock.py
+++ autodock.py
@@ -3,7 +3,7 @@ v = Vina(sf_name='vina')
 ligand_files = ['../Desktop/ligand-5kir.pdbqt']
 v.set_receptor("../Desktop/5kdir.pdbqt")
 v.set_ligand_from_file(ligand_files)
-v.compute_vina_maps(center=[centroid.x, centroid.y, centroid.z], box_size=[20, 20, 20])
+v.compute_vina_maps()
 v.dock(exhaustiveness=32, n_poses=20)
 file_name = files.split('.')[0]
 v.write_pose(file_name+"-resuts.pbqt")
