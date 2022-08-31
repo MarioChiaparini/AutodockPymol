c76a14ba4d7360b75d5f569105c557cecc9b0397
Mario_Chiaparini <45772937+MarioChiaparini@users.noreply.github.com>
2022-08-31 13:26:39 -0300

    Update autodock.py

--- autodock.py
+++ autodock.py
@@ -1,7 +1,7 @@
 from vina import Vina 
 v = Vina(sf_name='vina')
-ligand_files = ['/home/ABTLUS/mario.neto/Desktop/ligand-5kir.pdbqt']
-v.set_receptor("/home/ABTLUS/mario.neto/Desktop/5kdir.pdbqt")
+ligand_files = ['../Desktop/ligand-5kir.pdbqt']
+v.set_receptor("../Desktop/5kdir.pdbqt")
 v.set_ligand_from_file(ligand_files)
 v.compute_vina_maps(center=[centroid.x, centroid.y, centroid.z], box_size=[20, 20, 20])
 v.dock(exhaustiveness=32, n_poses=20)
